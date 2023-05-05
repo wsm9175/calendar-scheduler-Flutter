6b1589ea12876a1813698e864fd516e0d7720224
wsm9175 <wsm9175@naver.com>
2023-05-05 23:09:21 +0900

    add today banner

--- lib/component/calendar.dart
+++ lib/component/calendar.dart
@@ -2,15 +2,17 @@ import 'package:calendar_scheduler/const/colors.dart';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class Calendar extends StatefulWidget {
-  const Calendar({Key? key}) : super(key: key);
+class Calendar extends StatelessWidget {
+  final DateTime? selectedDay;
+  final DateTime focusedDay;
+  final OnDaySelected? onDaySelected;
 
-  @override
-  State<Calendar> createState() => _CalendarState();
-}
-
-class _CalendarState extends State<Calendar> {
-  DateTime? selectedDay;
+  const Calendar({
+    required this.selectedDay,
+    required this.focusedDay,
+    required this.onDaySelected,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,13 +21,13 @@ class _CalendarState extends State<Calendar> {
       borderRadius: BorderRadius.circular(6.0),
     );
     final defalutTextStyle = TextStyle(
-        color: Colors.grey[600],
-        fontWeight: FontWeight.w700,
+      color: Colors.grey[600],
+      fontWeight: FontWeight.w700,
     );
 
-
     return TableCalendar(
-      focusedDay: DateTime.now(),
+      locale: "ko_KR",
+      focusedDay: focusedDay,
       firstDay: DateTime(1800),
       // 최소 날짜 선정
       lastDay: DateTime(3000),
@@ -49,21 +51,15 @@ class _CalendarState extends State<Calendar> {
               border: Border.all(
                 color: PRIMARY_COLOR,
                 width: 1.0,
-              )
-          ),
+              )),
           defaultTextStyle: defalutTextStyle,
           weekendTextStyle: defalutTextStyle,
-          selectedTextStyle: defalutTextStyle.copyWith(
-            color: PRIMARY_COLOR
-          ),
-      ),
+          selectedTextStyle: defalutTextStyle.copyWith(color: PRIMARY_COLOR),
+          outsideDecoration: BoxDecoration(
+            shape: BoxShape.rectangle,
+          )),
 
-      onDaySelected: (DateTime selectedDay, DateTime focusedDay) {
-        print(selectedDay);
-        setState(() {
-          this.selectedDay = selectedDay;
-        });
-      },
+      onDaySelected: onDaySelected,
 
       selectedDayPredicate: (DateTime date) {
         if (selectedDay == null) return false;
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'screen/home_screen.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async{
+  //flutter framework의 준비 여부 체크
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //intl
+  await initializeDateFormatting();
+
   runApp(
     MaterialApp(
       home: HomeScreen(),
